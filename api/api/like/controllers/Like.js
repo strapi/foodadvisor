9c33e0ae64614a5a594afab487893940d515e366
Jim LAURIE <jim@strapi.io>
2019-02-20 10:39:36 +0100

    Add validations for likes

--- api/api/like/controllers/Like.js
+++ api/api/like/controllers/Like.js
@@ -49,6 +49,18 @@ module.exports = {
    */
 
   create: async (ctx) => {
+    // Set the reviewer ID as author key based on the login user info.
+    // https://strapi.io/documentation/3.x.x/guides/authentication.html#user-object-in-strapi-context
+    try {
+      ctx.request.body.author = ctx.state.user.id
+    } catch (err) {
+      return ctx.badRequest(null, 'Can\'t find authenticated user ID');
+    }
+
+    if (!ctx.request.body.review) {
+      return ctx.badRequest(null, '`review` attribute is missing');
+    }
+
     return strapi.services.like.add(ctx.request.body);
   },
 
