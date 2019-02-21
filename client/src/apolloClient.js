4cbf1d3b7f63b7598097d7ad52ec86744d4033cc
soupette <cyril.lpz@gmail.com>
2019-02-21 15:23:56 +0100

    Created Query component for easier testing

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'; // eslint-disable-line import/no-ex
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: 'http://localhost:1337/graphql'
+  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
 });
 const client = new ApolloClient({
   cache,
---
+++ client/src/components/Query/tests/Query.test.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import wait from 'waait';
+import { MockedProvider } from 'react-apollo/test-utils';
+
+import { GET_RESTAURANTS } from '../../../queries';
+import Query from '../index';
+
+const variables = {
+  limit: 1,
+  start: 0,
+  sort: 'name:ASC'
+};
+const props = { query: GET_RESTAURANTS, variables, render: null };
+const mocks = [
+  {
+    request: {
+      query: GET_RESTAURANTS,
+      variables
+    },
+    result: {
+      data: {
+        restaurants: [
+          {
+            id: '1',
+            name: 'restaurant',
+            description: 'A cool restaurant',
+            cover: {
+              url: '/uploads/5002461eee1c4c87b2e6eb558eead789.png'
+            }
+          }
+        ]
+      }
+    }
+  }
+];
+
+describe('<Query />', () => {
+  it('should render a loading state', () => {
+    const component = renderer.create(
+      <MockedProvider mocks={[]}>
+        <Query {...props} />
+      </MockedProvider>
+    );
+    const tree = component.toJSON();
+
+    expect(tree.children).toContain('Loading');
+  });
+
+  it('should call the render props', async () => {
+    const render = jest.fn(() => null);
+    renderer.create(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Query {...props} render={render} />
+      </MockedProvider>
+    );
+
+    await wait(0); // wait for response
+
+    expect(render).toHaveBeenCalled();
+  });
+
+  it('should return null if no render method is given', async () => {
+    const component = renderer.create(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Query {...props} />
+      </MockedProvider>
+    );
+
+    await wait(0); // wait for response
+
+    const tree = component.toJSON();
+
+    expect(tree).toBeNull();
+  });
+
+  it('should show error UI', async () => {
+    const restaurantsMock = {
+      request: {
+        query: GET_RESTAURANTS,
+        variables: {
+          limit: 1,
+          start: 0,
+          sort: 'name:ASC'
+        }
+      },
+      error: new Error('aw shucks')
+    };
+
+    const component = renderer.create(
+      <MockedProvider mocks={[restaurantsMock]} addTypename={false}>
+        <Query {...props} />
+      </MockedProvider>
+    );
+
+    await wait(0); // wait for response
+
+    const tree = component.toJSON();
+    expect(tree.children).toContain('Error');
+  });
+});
