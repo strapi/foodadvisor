9be5f2da8081766a280558a650cd0c44f46ffb4e
Jim LAURIE <jim@strapi.io>
2019-03-01 14:41:14 +0100

    Add category and restaurant data seed

--- api/config/functions/bootstrap.js
+++ api/config/functions/bootstrap.js
@@ -30,6 +30,41 @@ module.exports = async (cb) => {
 
     await strapi.plugins['users-permissions'].services.userspermissions.updateRole(publicRoleId, publicRole);
     await strapi.plugins['users-permissions'].services.userspermissions.updateRole(authenticatedRoleId, authenticatedRole);
+
+    await Promise.all(strapi.config.categories.map(async (categoryName) => {
+      let categories = await strapi.api.category.services.category.fetchAll({
+        name: categoryName
+      });
+
+      categories = categories.toJSON();
+
+      if (categories.length === 0) {
+        await strapi.api.category.services.category.add({
+          name: categoryName
+        });
+      }
+    }));
+
+    await Promise.all(strapi.config.restaurants.map(async (restaurant) => {
+      let restaurants = await strapi.api.restaurant.services.restaurant.fetchAll({
+        name: restaurant.name
+      });
+
+      restaurants = restaurants.toJSON();
+
+      if (restaurants.length === 0) {
+        let category = await strapi.api.category.services.category.fetchAll({
+          name: restaurant.category
+        });
+
+        category = category.toJSON();
+        category = category[0];
+
+        restaurant.category = category;
+
+        await strapi.api.restaurant.services.restaurant.add(restaurant);
+      }
+    }));
   } catch (err) {
     console.log('⚠️ An error occured during the application bootstrap');
     console.log();
