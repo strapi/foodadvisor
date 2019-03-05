7f7b415cefb26625059f6a39c250ebc9d91c506f
Alexandre Bodin <bodin.alex@gmail.com>
2019-03-05 18:49:33 +0100

    Adds count to the reviews and count + note to the restaurants apis

---
+++ api/api/restaurant/config/schema.graphql.js
@@ -0,0 +1,37 @@
+module.exports = {
+  definition: /* GraphQL */ `
+    extend type Restaurant {
+      note: Float
+    }
+
+    type RestaurantsConnection {
+      aggregate: RestaurantsAggregate
+    }
+
+    type RestaurantsAggregate {
+      count: Int
+    }
+  `,
+  query: /* GraphQL */ `
+    restaurantsConnection(where: JSON): RestaurantsConnection
+  `,
+  resolver: {
+    Query: {
+      restaurantsConnection(_, args) {
+        return args;
+      },
+    },
+    RestaurantsConnection: {
+      aggregate(args) {
+        return args;
+      },
+    },
+    RestaurantsAggregate: {
+      count(args) {
+        return strapi.controllers.restaurant.count({
+          query: args.where || {},
+        });
+      },
+    },
+  },
+};
