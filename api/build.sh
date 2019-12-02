0e494b3df8bceac98aa45402122cbf330d6cf6eb
chadcarlson <chad.carlson@platform.sh>
2019-12-02 13:40:18 -0500

    Update download commands.

--- api/build.sh
+++ api/build.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-wget https://yarnpkg.com/latest.tar.gz
-tar zvxf latest.tar.gz
+YARN_VERSION=1.19.2
 
-rm latest.tar.gz
-YARN_PATH=$(find . -name \*yarn\* -type d -maxdepth 1 -print | head -n1)
+wget https://github.com/yarnpkg/yarn/releases/download/v$YARN_VERSION/yarn-v${YARN_VERSION}.tar.gz
+tar zvxf yarn-v$YARN_VERSION.tar.gz
 
-echo $YARN_PATH
+rm yarn-v$YARN_VERSION.tar.gz
 
-IFS='/' read -ra my_array <<< "$YARN_PATH"
-
-export PATH="$PATH:${my_array[1]}/bin"
+export PATH="$PATH:yarn-v$YARN_VERSION/bin"
 
 yarn
 yarn run seed
