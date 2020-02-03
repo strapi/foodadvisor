86b0e0c4a00aa16ef2fb89a0230686bebe1d5d0d
Pierre Burgy <pierre.burgy@gmail.com>
2020-02-03 08:33:49 -0800

    Update url in email

--- api/scripts/send-email.js
+++ api/scripts/send-email.js
@@ -2,7 +2,7 @@ const config = require('platformsh-config').config();
 const http = require('http');
 
 // Platform.sh
-const projectUrl = `${config.getPrimaryRoute().url}admin`;
+const projectUrl = `${config.getPrimaryRoute().url}`;
 const email = config.branch;
 
 // HubSpot
