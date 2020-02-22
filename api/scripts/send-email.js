77697a5031cda9f3902bac39ee6f107b31ef51b8
Pierre Burgy <pierre.burgy@gmail.com>
2020-02-22 16:09:56 +0100

    Fix typo

--- api/scripts/send-email.js
+++ api/scripts/send-email.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 // Platform.sh
 const projectUrl = `${config.getPrimaryRoute().url}`;
-const email = config.variablesDefconfig && config.variablesDef.EMAIL;
+const email = config.variablesDef && config.variablesDef.EMAIL;
 
 // HubSpot
 const hubSpotProjectId = '6893032';
