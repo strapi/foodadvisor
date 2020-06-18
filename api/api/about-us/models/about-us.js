9ae081c9941053cf0ca21a43250f87c10e2d6c14
Jim LAURIE <j.laurie6993@gmail.com>
2020-06-18 15:42:20 +0200

    Add Single Type + content for about-us page

---
+++ api/api/about-us/models/about-us.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#life-cycle-callbacks)
+ * to customize this model
+ */
+
+module.exports = {};
