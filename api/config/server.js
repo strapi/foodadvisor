7235c41b8aaf501ff78a5637879faf41573c1c45
Pierre Burgy <pierre.burgy@gmail.com>
2020-07-19 17:37:58 +0200

    Fix server.json file
    Signed-off-by: Pierre Burgy <pierre.burgy@gmail.com>

--- api/config/server.js
+++ api/config/server.js
@@ -1,4 +1,9 @@
 module.exports = ({ env }) => ({
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1338358585'),
+    },
+  },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
 });
