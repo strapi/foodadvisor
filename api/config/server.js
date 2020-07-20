90d57cecd6b28e6783019c14a4c80a8070cff4c7
Pierre Burgy <pierre.burgy@gmail.com>
2020-07-20 14:19:51 +0200

    Remove default auth secret value
    Signed-off-by: Pierre Burgy <pierre.burgy@gmail.com>

--- api/config/server.js
+++ api/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '1338358585'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
   host: env('HOST', '0.0.0.0'),
