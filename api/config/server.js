a2a36ca6d53e25582fcbc199a50ea5bf3c6929fe
Rémi de Juvigny <remi@hey.com>
2021-02-20 23:53:01 +0100

    Don't host admin

--- api/config/server.js
+++ api/config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
+    serverAdminPanel: false,
   },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
