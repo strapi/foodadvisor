966a418914fcee5cdb6d83d2766f1db173d852a5
Maxime Castres <mcastres@student.42.fr>
2021-12-13 18:44:56 +0100

    Remove log CT

--- api/src/api/log/controllers/log.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * A set of functions called "actions" for `log`
- */
-
-const { createCoreController } = require("@strapi/strapi").factories;
-
-module.exports = createCoreController("api::log.log");
--- api/src/api/log/services/log.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * log service.
- */
-
-const { createCoreService } = require("@strapi/strapi").factories;
-
-module.exports = createCoreService("api::log.log");
