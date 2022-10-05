2cec5fe3e276051670cea738f105f2f748b5c4b2
Maxime Castres <mcastres@student.42.fr>
2022-10-05 10:58:23 +0200

    Chore: Derrick's review

--- api/config/cron-tasks.js
+++ api/config/cron-tasks.js
@@ -1,33 +1,35 @@
-module.exports = {
-  '*/1 * * * *': ({ strapi }) => {
-    const contentTypes = [
-      'api::article.article',
-      'api::restaurant.restaurant',
-      'api::page.page',
-    ];
+// module.exports = {
+//   '*/1 * * * *': ({ strapi }) => {
+//     const contentTypes = [
+//       'api::article.article',
+//       'api::restaurant.restaurant',
+//       'api::page.page',
+//     ];
 
-    contentTypes.map(async (type) => {
-      try {
-        const draftArticlesToPublish = await strapi.db.query(type).findMany({
-          where: {
-            publicationState: 'Publication scheduled',
-            publishedAt: null,
-            publish_at_lt: new Date(),
-          },
-        });
+//     contentTypes.map(async (type) => {
+//       try {
+//         const draftArticlesToPublish = await strapi.entityService.findMany(
+//           type,
+//           {
+//             where: {
+//               publicationState: 'Publication scheduled',
+//               publishedAt: null,
+//               publish_at_lt: new Date(),
+//             },
+//           }
+//         );
 
-        await Promise.all(
-          draftArticlesToPublish.map(async (article) => {
-            return await strapi.db.query(type).update({
-              where: { id: article.id },
-              data: {
-                publishedAt: new Date(),
-                publicationState: 'Published',
-              },
-            });
-          })
-        );
-      } catch (error) {}
-    });
-  },
-};
+//         await Promise.all(
+//           draftArticlesToPublish.map(async (article) => {
+//             return await strapi.entityService.update(type, article.id, {
+//               data: {
+//                 publishedAt: new Date(),
+//                 publicationState: 'Published',
+//               },
+//             });
+//           })
+//         );
+//       } catch (error) {}
+//     });
+//   },
+// };
